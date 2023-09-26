806964e7c707d74747b653f3167e22fd00554666
Alberth henrique <alberthhls@protonmail.com>
2023-09-26 09:38:40 -0300

    Update sender.controller.js

--- controllers/sender.controller.js
+++ controllers/sender.controller.js
@@ -1,13 +1,13 @@
-const accountSId = "AC917eb904a36dde201659ce756a064911";
-const AuthToken = "e8b79b6836b9e05d4c5841a280b82587";
+const accountSId = "";
+const AuthToken = "";
 
 const client = require('twilio')(accountSId, AuthToken);
 
 sender = (body) => {
     target = 
     [
-        {nome: "alberth", num : "+556183360091"},
-        {nome: "alberth", num : "+556183360091"},
+        {nome: "alberth", num : "+"},
+        {nome: "alberth", num : "+"},
     ]
 
     var acc, received, reject, arr = target;
@@ -18,7 +18,7 @@ sender = (body) => {
            (function(arr, i){
                 setInterval(function(){
                     client.messages.create({    
-                        from: 'whatsapp:+14155238886',
+                        from: 'whatsapp:+',
                         body: body,
                         to: 'whatsapp:' + arr[i].num})
                             .then(message => {
